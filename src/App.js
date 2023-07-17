fd1bd30e014ad669a49fe4321ce80e84690f9856
MohamedMaamori <mohamed.maamori67@gmail.com>
2023-07-17 16:44:29 +0200

    Add components: Hello and Identity

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Hello from './components/Hello'
+import Identity from "./components/Identity";
 
 function App() {
+
+
   return (
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Hello world .
+          Edit <code>src/App.js</code>
         </p>
         <a
           className="App-link"
@@ -17,6 +22,11 @@ function App() {
         >
           Learn React
         </a>
+          <Hello/>
+          <Identity
+              name={'Mohamed'}
+              bio={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat'}
+              />
       </header>
     </div>
   );
---
+++ src/components/Hello.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {render} from "@testing-library/react";
+
+
+export default function hello(){
+       const dwwm = "Hello les DWWM"
+       return(
+           <div>
+               {dwwm}
+           </div>
+
+       );
+}
